8956b118d8d4ade895e66af9116187b9ec9c93f4
zlcj <gg460715753@gmail.com>
2018-11-13 23:05:22 +0800

    路由

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+        <a v-link="{path:'/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/seller'}">商家</a>
+      </div>
     </div>
-    <div class="contnet">
-      I am content!
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-  import header from './components/header/header.vue'
+  import header from 'components/header/header.vue'
 
   export default {
     components: {
@@ -33,4 +37,10 @@
       .tab-item
         flex: 1
         text-align: center
+        & > a
+          display: block
+          font-size: 14px
+          color: rgb(77, 85, 93)
+          &.active
+            color: rgb(240, 20, 20)
 </style>
